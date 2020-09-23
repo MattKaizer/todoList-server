ec15eba74175616ac16fafbfde6620c00c12836b
Matías Benjamín Moreno Juárez <matt@iMac-de-Matias.home>
2020-09-23 16:22:52 +0100

    create/add tasks

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,6 +1,7 @@
 const Task = require('../models/Task');
 const Project = require('../models/Projects');
 const { validationResult } = require('express-validator');
+const Projects = require('../models/Projects');
 
 exports.createTask = async (req, res) => {
 
@@ -10,6 +11,28 @@ exports.createTask = async (req, res) => {
         return res.status(400).json({errors:errors.array()})
     }
 
+    // check if project exist
+
+    
+    try {
+        const { project } = req.body;
+        const existentProject = await Projects.findById(project);
+        if (!existentProject) {
+            return res.status(404).json({msg: 'Proyecto no encontrado'});
+        }
+        // check project owner
+        if (existentProject.author.toString() !== req.user.id) {
+            res.status(401).json({msg: 'No autorizado'});
+        }
+
+        // create task
+        const task = new Task(req.body);
+        await task.save();
+        res.json({task});
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
     
 
 }
