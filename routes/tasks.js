349c6bd1e7c5008980b0dd40b016841d96d57963
Matías Benjamín Moreno Juárez <matt@iMac-de-Matias.local>
2020-10-03 12:39:21 +0100

    fix script for heroku 4

---
+++ routes/tasks.js
@@ -0,0 +1,40 @@
+/* Routes for create tasks */
+const express = require('express');
+const router = express.Router();
+
+const taskController = require('../controllers/taskController');
+const auth = require('../middleware/auth');
+const { check } = require('express-validator');
+
+// /api/tasks
+router.post('/',
+    auth,
+    [
+        check('name', 'El nombre es obligatorio.').notEmpty(),
+        check('project', 'El proyecto es obligatorio.').notEmpty()
+    ],
+    taskController.createTask
+);
+
+// /api/tasks - get all task
+router.get('/',
+    auth,
+    taskController.getTasks
+)
+
+// update
+router.put('/:id', 
+    auth,
+    // [
+    //     check('name', 'El nombre de la tarea es obligatorio.').notEmpty()
+    // ],
+    taskController.updateTask
+)
+
+// delete
+router.delete('/:id',
+    auth,
+    taskController.deleteTask
+)
+
+module.exports = router;
